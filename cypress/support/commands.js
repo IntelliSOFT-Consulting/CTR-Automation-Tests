a486707cdd543d82b2f325999ac503d0127e6a2a
Muyundo <muyundobrian@gmail.com>
2025-02-25 16:17:04 +0300

    updated logins

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,27 +1,17 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
+
 Cypress.Commands.add("baseurl", () => {
 
-    cy.visit('http://45.79.161.190:8080/', { waitUntil: 'domcontentloaded' });
+    cy.visit('http://45.79.161.190:8080/', { waitUntil: 'domcontentloaded' })
 })
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", () => {
+    cy.fixture('config.json').then((config) => {
+        cy.wrap(config).as('configData')  // Store in an alias
+    })
+
+  /*  cy.get('@configData').then((config) => {
+        cy.get('#UserUsername').type(config.username)
+        cy.get('#UserPassword').type(config.password)
+        cy.contains('Sign in').click()
+    })*/
+})
+
