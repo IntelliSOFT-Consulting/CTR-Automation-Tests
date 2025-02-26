c86ecf153fb41e059607f405aa15bd26d43f92ad
Muyundo <muyundobrian@gmail.com>
2025-02-26 10:03:13 +0300

    configs.json

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,7 +4,7 @@ Cypress.Commands.add("baseurl", () => {
     cy.visit('http://45.79.161.190:8080/', { waitUntil: 'domcontentloaded' })
 })
 Cypress.Commands.add("login", () => {
-    cy.fixture('config.json').then((config) => {
+    cy.fixture('configs.json').then((config) => {
         cy.wrap(config).as('configData')  // Store in an alias
     })
 
