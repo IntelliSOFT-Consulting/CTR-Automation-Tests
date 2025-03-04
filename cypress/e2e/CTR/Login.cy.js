55abf45a2cc8703e46467df0114f6ff7400ade96
Muyundo <muyundobrian@gmail.com>
2025-03-04 14:18:45 +0300

    updated configs naming from config

--- cypress/e2e/CTR/Login.cy.js
+++ cypress/e2e/CTR/Login.cy.js
@@ -2,8 +2,8 @@
 
 describe('Test Suite', function () {  
     beforeEach(function () {  
-      cy.fixture('config.json').then((config) => {
-        this.config = config  // Store fixture data in `this`
+      cy.fixture('configs.json').then((configs) => {
+        this.configs = configs  // Store fixture data in `this`
       })
       cy.baseurl()
     })
@@ -15,14 +15,14 @@ describe('Test Suite', function () {
     it('Verify that a user cannot login with incorrect username & correct password', function () {
       cy.get('.navbar-inner > .container').contains('Login').click()
       cy.get('#UserUsername').type('rian')
-      cy.get('#UserPassword').type(this.config.password) 
+      cy.get('#UserPassword').type(this.configs.password) 
       cy.contains('Sign in').click()
       cy.contains('Your username or password was incorrect').should('be.visible')
     })
   
     it('Verify that a user cannot login with correct username & incorrect password', function () {
       cy.get('.navbar-inner > .container').contains('Login').click()
-      cy.get('#UserUsername').type(this.config.username) // Use `this.config`
+      cy.get('#UserUsername').type(this.configs.username) // Use `this.config`
       cy.get('#UserPassword').type('wrongpassword')
       cy.contains('Sign in').click()
       cy.contains('Your username or password was incorrect').should('be.visible')
@@ -36,8 +36,8 @@ describe('Test Suite', function () {
   
     it('Verify that a user can login with correct username and correct password', function () {
       cy.get('.navbar-inner > .container').contains('Login').click()
-      cy.get('#UserUsername').type(this.config.username) // Use `this.config`
-      cy.get('#UserPassword').type(this.config.password)
+      cy.get('#UserUsername').type(this.configs.username) // Use `this.config`
+      cy.get('#UserPassword').type(this.configs.password)
       cy.contains('Sign in').click()
       cy.contains('Pre-Submission Meeting').should('be.visible')
     })
